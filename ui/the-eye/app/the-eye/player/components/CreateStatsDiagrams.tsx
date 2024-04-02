f92dac146434a883d83e628a4297bb66f70c614c
bradenmosley <144081350+bradenmosley@users.noreply.github.com>
2024-04-02 16:56:23 -0500

    More updates

--- ui/the-eye/app/the-eye/player/components/CreateStatsDiagrams.tsx
+++ ui/the-eye/app/the-eye/player/components/CreateStatsDiagrams.tsx
@@ -1,5 +1,5 @@
 import { prisma } from '@/app/utils/db';
-import { batter_stats } from "@/app/utils/types";
+import { batter_stats, pitcher_stats } from "@/app/utils/types";
 import { batter_replacer } from '@/app/utils/replacer';
 import Box from '@mui/material/Box';
 import BattingStatsBarChart from './BattingStatsBarChart';
@@ -10,11 +10,20 @@ export default async function CreateStatsDiagrams(
     // https://www.prisma.io/docs/orm/more/upgrade-guides/upgrading-versions/upgrading-to-prisma-4#raw-query-mapping-postgresql-type-casts
     // In SQL function, get_batter_stats takes the params (text, text, date, date)
     // ::date explicitly casts the startDate and endDate strings to a SQL date type
-    const batters = await prisma.$queryRaw<batter_stats[]>`SELECT * FROM get_batter_stats(${player}, ${team}, ${startDate}::date, ${endDate}::date)`;
     
-    return (
-        <Box>
-            <BattingStatsBarChart player = {JSON.parse(JSON.stringify(batters, batter_replacer))}/>
-        </Box>
-    );
+    const batters = await prisma.$queryRaw<batter_stats[]>`SELECT * FROM get_batter_stats(${player}, ${team}, ${startDate}::date, ${endDate}::date)`;
+    if (batters.length != 0) {
+        return (
+            <Box>
+                <BattingStatsBarChart player = {JSON.parse(JSON.stringify(batters, batter_replacer))}/>
+            </Box>
+        );
+    } else {
+        const pitchers = await prisma.$queryRaw<pitcher_stats[]>`SELECT * FROM get_pitcher_stats(${player}, ${team}, ${startDate}::date, ${endDate}::date)`;
+        return (
+            <Box>
+                <h2>Pitcher Stats</h2>
+            </Box>
+        );
+    }
 }
--- ui/the-eye/app/the-eye/team/components/CreatePitcherTable.tsx
+++ ui/the-eye/app/the-eye/team/components/CreatePitcherTable.tsx
@@ -4,7 +4,7 @@ import { pitcher_stats } from '@/app/utils/types';
 import PitcherTable from './PitcherTable';
 
 export default async function CreatePitcherTable({ team }: { team: string }) {    
-    const pitchers = await prisma.$queryRaw<pitcher_stats[]>`SELECT * FROM pitcher_stats_view WHERE "PitcherTeam" = ${team}`;
+    const pitchers = await prisma.$queryRaw<pitcher_stats[]>`SELECT * FROM pitcher_stats_view_2024 WHERE "PitcherTeam" = ${team}`;
 
     return (
         <PitcherTable players={JSON.parse(JSON.stringify(pitchers, pitcher_replacer))}/>
