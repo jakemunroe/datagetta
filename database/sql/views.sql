ecbee6c79b1cb4056ac1a42526a4373f769aef98
Caden Garrett <garrettcs11@gmail.com>
2024-04-03 10:10:46 -0500

    Change

--- database/sql/views.sql
+++ database/sql/views.sql
@@ -227,6 +227,6 @@ select "Pitcher", "PitcherTeam",
         AVG("VertApprAngle") as avg_vert_appr_angle,
         AVG("HorzApprAngle") as avg_horz_appr_angle
 from trackman_pitcher
-where "AutoPitchType" != null
+where "AutoPitchType" != ''
 group by ("Pitcher", "PitcherTeam", "PitcherThrows", "PitchType");
     
